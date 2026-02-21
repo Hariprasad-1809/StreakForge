15e8db16d07244e56879e8c7105aaa9b469fc12a
HARIPRASAD H <hariprasadh1809@gmail.com>
2026-02-21 09:46:53 +0530

    chan

--- backend/main.py
+++ backend/main.py
@@ -44,7 +44,7 @@ origins = [origin.strip() for origin in allowed_origins.split(",") if origin.str
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins=["*"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
