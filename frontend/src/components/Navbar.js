b73faf1cbe20549371f0d9c21075e25f79234e53
HARIPRASAD H <hariprasadh1809@gmail.com>
2026-02-22 12:16:03 +0530

    Update Navbar.js

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,10 +1,12 @@
 import { Link } from "react-router-dom";
-
+import logo from "../streak.jpeg";
 export default function Navbar() {
   return (
     <nav className="navbar">
-      <div className="nav-logo">🚀 StreakForge</div>
-
+      <div className="nav-logo">
+          <img src={logo} alt="StreakForge Logo" classname="logo-img"/>
+      </div>
+      
       <div className="nav-links">
         <Link to="/">Home</Link>
         <Link to="/about">About</Link>
