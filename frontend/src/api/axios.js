18ca525543235eaf83e07ad9be673a1715f44597
HARIPRASAD H <hariprasadh1809@gmail.com>
2026-02-22 11:38:26 +0530

    Update axios.js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,14 +1,16 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.REACT_APP_API_BASE_URL,
+  baseURL: process.env.REACT_APP_API_BASE_URL,
 });
 
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
+
   if (token) {
-    config.headers.Authorization = 'Bearer ${token}';
+    config.headers.Authorization = Bearer ${token};
   }
+
   return config;
 });
 
